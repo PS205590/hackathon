ff7398b3c6d2d9c0cfc820c8510baf3a47682caf
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2024-12-03 09:26:10 +0100

    initial commit

---
+++ hackathon/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('base')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Dit is de homepagina voor de Hackathon.</h1>
+            </div>
+        </div>
+    </div>
+@endsection
